abf238822c48489c75266c6e0c49c0d29b7a5484
Joe Abraham <joseph.abraham2@outlook.com>
2022-06-20 06:28:11 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 http_proxy=http://tstark:pssecrocks@172.31.245.222:8888 apt-get update
 http_proxy=http://tstark:pssecrocks@172.31.245.222:8888 apt install -y nano
+http_proxy=http://tstark:pssecrocks@172.31.245.222:8888 apt install -y jq
